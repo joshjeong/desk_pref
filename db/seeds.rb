a506c82e3eb5ae406e6d3cf6e529738dba7d0460
Josh Jeong <jcjeong@gmail.com>
2014-11-10 14:53:34 -0800

    Changes mega desc

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 
 Desk.create(style: "basic", price: 50, description: "Perfect for coloring spreadsheets, Easy to assemble, 35.63 x 23.5 x 24")
 Desk.create(style: "standing", price: 300, description: "No more being hunched over, Adjustable height, 59 x 28.5 x 33.5-45.25")
-Desk.create(style: "mega", price: 2000, description: "6 monitors with Bose speakers, For Developers only, 60 x 30 x 30")
+Desk.create(style: "mega", price: 2000, description: "Large touchscreen for debugging, For Developers only, 60 x 30 x 30")
