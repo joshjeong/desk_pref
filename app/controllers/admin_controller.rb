e642048ad8e4d3824254b24b670b1fc6cd0cece6
Josh Jeong <jcjeong@gmail.com>
2014-11-11 18:28:13 -0800

    Updates model for users admin privileges

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,20 @@
 class AdminController < ApplicationController
   def index
-    users = User.all
+    @users = User.all
     @users_list = {}
-    users.each do |user|
-      email = user.email.split('@').first
-      @users_list[email] = user.admin
+    @users.each do |user|
+      @users_list[user.email] = user.admin
     end
     @users_list
   end
+
+  def update
+    email = params[:email]
+    admin = params[:admin]
+    @user = User.find_by('email = ?', email)
+    @user.admin = admin
+    @user.save
+    render :updated, layout: false
+  end
+
 end
