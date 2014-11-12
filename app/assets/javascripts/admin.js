f2d379a337a5a918555083621e92457be87e4b9d
Josh Jeong <jcjeong@gmail.com>
2014-11-11 18:27:45 -0800

    Toggles on/off switch to assign user admin rights

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -9,29 +9,91 @@ Admin.Controller = function(view){
 
 Admin.Controller.prototype = {
   bindListeners: function(){
-    this.dataNavListener();
-    this.usersNavListener();
+    this.dataTabListener();
+    this.usersTabListener();
+    this.toggleOnListener();
+    this.toggleOffListener();
   },
-  dataNavListener: function(){
+  dataTabListener: function(){
     var self = this;
     $('#data-nav').on('click', function(){
-      self.dataNav();
+      self.dataTab();
     })
   },
-  usersNavListener: function(){
+  usersTabListener: function(){
     var self = this;
     $('#users-nav').on('click', function(){
-      self.usersNav();
+      self.usersTab();
+    })
+  }, 
+  toggleOnListener: function(){
+    var self = this;
+    $('.admin-setting-column').on('click', '.admin-false', function(){
+      self.toggleOn($(this));
     })
   },
-  dataNav: function(){
+  toggleOffListener: function(){
+    var self = this;
+    $('.admin-setting-column').on('click', '.admin-true', function(){
+      self.toggleOff($(this));
+    })
   },
-  usersNav: function(){
+  dataTab: function(){
+    this.view.hideUsersTab();
+    this.view.showDataTab();
+  },
+  usersTab: function(){
+    this.view.hideDataTab();
+    this.view.showUsersTab();
+  },
+  toggleOn: function(button){
+    var email = button.closest('tr').find('.user-column').html().trim()
+    console.log(email)
+    this.view.toggleOnPic(button)
+    $.ajax({
+      url: "/admin/update",
+      type: "PUT",
+      data: {email: email, admin: true}
+    }).done(function(response){
+
+    })
+  },
+  toggleOff: function(button){
+    var email = button.closest('tr').find('.user-column').html().trim()
+    this.view.toggleOffPic(button);
+    $.ajax({
+      url: "/admin/update",
+      type: "PUT",
+      data: {email: email, admin: false}
+    }).done(function(response){
+
+    })
   }
 }
 
 Admin.View = function(){}
 
 Admin.View.prototype = {
-
+  toggleOnPic: function(button){
+    button.css('background-image', "url('/assets/toggle-on.png')")
+    button.removeClass('admin-false')
+    button.addClass('admin-true')
+  },
+  toggleOffPic: function(button){
+    button.css('background-image', "url('/assets/toggle-off.png')")
+    button.removeClass('admin-true')
+    button.addClass('admin-false')
+  },
+  hideDataTab: function(){
+    $('.data-container').css('visibility', 'hidden')
+  },
+  showDataTab: function(){
+    $('.data-container').css('visibility', 'visible')
+  },
+  hideUsersTab: function(){
+    $('.users-container').css('visibility', 'hidden')
+  },
+  showUsersTab: function(){
+    $('.users-container').css('visibility', 'visible')
+  }
 }
