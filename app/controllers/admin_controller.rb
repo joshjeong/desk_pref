e6574d8d7fb6f94b4ff5afe99dc97f73f1962ec8
Josh Jeong <jcjeong@gmail.com>
2014-11-12 12:16:01 -0800

    Returns data from model to display

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -27,6 +27,24 @@ class AdminController < ApplicationController
   end
 
   def hover
+    desk = params[:desk]
+    desk_price = Desk.find_by('style=?', desk).price
+    all_users = User.all.length
+    desk = desk.upcase!
+    desk_users = User.where('desk_type = ?', desk).length
+    count = count_desks(get_desk_types)
+    calc = calc_perc(count[desk], count["sum"])
+    percentage = calc.round(2).to_s+"%"
+    data = {
+      price: desk_price,
+      allUsers: all_users,
+      deskUsers: desk_users,
+      count: count,
+      percentage: percentage      
+    }
+    respond_to do |format|
+      format .json {render json: data}
+    end
   end
 
 end
